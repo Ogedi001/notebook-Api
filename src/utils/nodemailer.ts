363d150ae8c25b71df712e34839453f436025990
ogedi001 <ogedifavour2@gmail.com>
2024-04-14 01:01:00 +0100

    chage smtp port

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -4,7 +4,7 @@ import 'dotenv/config'
 const config={
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    
+    secure: true,
     auth: {
       user: process.env.SMTP_USERNAME,
       pass: process.env.SMTP_PASSWORD,
