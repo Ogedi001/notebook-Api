8fd71a67787cf2ea50b865c37d540091b030d151
ogedi001 <ogedifavour2@gmail.com>
2024-04-15 12:55:22 +0100

    modified notebook test

--- src/test/notebook-controller.test.ts
+++ src/test/notebook-controller.test.ts
@@ -13,53 +13,57 @@ import { StatusCodes } from "http-status-codes";
 
 jest.mock("./../service/notebook-service.ts");
 jest.mock("../utils/redis.ts", () => ({
-    redisClient: jest.fn(),
-  }));
+  redisClient: jest.fn(),
+}));
 //arrange, act,assert
-
 
 const mockNotebook: NoteBook = {
   id: "1",
   title: "Test Notebook",
   content: "Test Content",
-  userId:"userss",
-  privacy:"PRIVATE",
+  userId: "userss",
+  privacy: "PRIVATE",
   createdAt: new Date(),
   updatedAt: new Date(),
 };
 
 describe("Notebook Controller", () => {
-    describe("createNotebookController", () => {
-      it("It should create a new Notebook sucessfully", async () => {
-        const req = {
-          body: { title: "Test Notebook", content: "Test Content" },
-        } as Request;
+  describe("createNotebookController", () => {
+    it("It should create a new Notebook sucessfully", async () => {
+      const req = {
+        body: { title: "Test Notebook", content: "Test Content" },
+      } as Request;
 
-        const res = {
-          status: jest.fn().mockReturnThis(),
-          json: jest.fn(),
-        } as unknown as Response;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response;
 
-        (
-          notebookService.createNoteBookService as jest.Mock
-        ).mockResolvedValueOnce(mockNotebook);
+      (
+        notebookService.createNoteBookService as jest.Mock
+      ).mockResolvedValueOnce(mockNotebook);
 
-       await createNoteBookController(req, res);
+      await createNoteBookController(req, res);
 
-        expect(res.status).toHaveBeenCalledWith(StatusCodes.CREATED);
-        expect(res.json).toHaveBeenCalledWith({
-          data:mockNotebook,
-          message: "success",
-        });
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.CREATED);
+      expect(res.json).toHaveBeenCalledWith({
+        data: mockNotebook,
+        message: "success",
       });
     });
+  });
 
   describe("getNoteBookByIdServiceController", () => {
     it("it should get existing notebook by Id", async () => {
-
-      (notebookService.findNoteBookByIdService as jest.Mock)
-        .mockResolvedValueOnce(mockNotebook);
-      const req = { params: { id: '1' } } as unknown as Request;
+      (
+        notebookService.findNoteBookByIdService as jest.Mock
+      ).mockResolvedValueOnce(mockNotebook);
+      const req = {
+        currentUser: {
+          id: "ddd",
+        },
+        params: { id: "1" },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
@@ -67,7 +71,7 @@ describe("Notebook Controller", () => {
       } as unknown as Response;
 
       await getNoteBookByIdServiceController(req, res);
-
+      expect(notebookService.findNoteBookByIdService).toHaveBeenCalledWith("1","ddd");
       expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
       expect(res.json).toHaveBeenCalledWith({
         data: mockNotebook,
@@ -80,34 +84,42 @@ describe("Notebook Controller", () => {
     it(" it should get all notebook and perform search query with notebook title", async () => {
       const mockNotebookArray: notebookService.NotebooksData = {
         pagination: {
-            page: 1,
-            limit: 10,
-            totalPages: 1,
-            totalNotebooks: 1,
+          page: 1,
+          limit: 10,
+          totalPages: 1,
+          totalNotebooks: 1,
         },
         notebooks: [
           {
             id: "1",
             title: "Test Notebook",
             content: "Test Content",
-            userId:"userss",
-            privacy:"PRIVATE",
+            userId: "userss",
+            privacy: "PRIVATE",
             createdAt: new Date(),
             updatedAt: new Date(),
           },
         ],
-    };
-    
+      };
 
       (
         notebookService.getNoteBooksQueryService as jest.Mock
       ).mockResolvedValueOnce(mockNotebookArray);
-      const req = { query: {} } as Request;
+      const req = {
+        currentUser: {
+          id: "ddd",
+        },
+        query: {},
+      } as Request;
       const res = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
       } as unknown as Response;
       await getAllNoteBooksQuerySearch(req, res);
+      expect(notebookService.getNoteBooksQueryService).toHaveBeenCalledWith(
+        req.query,
+        req.currentUser?.id!
+      );
       expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
       expect(res.json).toHaveBeenCalledWith({
         data: mockNotebookArray,
@@ -118,13 +130,16 @@ describe("Notebook Controller", () => {
 
   describe("updateNoteBookController", () => {
     it("it should update a notebook by id ", async () => {
-        (
-            notebookService.findNoteBookByIdService as jest.Mock
-          ).mockResolvedValueOnce(mockNotebook);
+      (
+        notebookService.findNoteBookByIdService as jest.Mock
+      ).mockResolvedValueOnce(mockNotebook);
       (
         notebookService.updateNoteBookService as jest.Mock
       ).mockResolvedValueOnce(mockNotebook);
       const req = {
+        currentUser: {
+          id: "ddd",
+        },
         params: { id: "1" },
         body: { title: "new title", content: "new content" },
       } as unknown as Request;
@@ -134,7 +149,8 @@ describe("Notebook Controller", () => {
       } as unknown as Response;
 
       await updateNoteBookController(req, res);
-
+      
+      expect(notebookService.findNoteBookByIdService).toHaveBeenCalledWith("1","ddd");
       expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
       expect(res.json).toHaveBeenCalledWith({
         data: mockNotebook,
@@ -145,13 +161,16 @@ describe("Notebook Controller", () => {
 
   describe("deleteNoteBookController", () => {
     it("it should delete a notebook by id", async () => {
-        (
-            notebookService.findNoteBookByIdService as jest.Mock
-          ).mockResolvedValueOnce(mockNotebook);
+      (
+        notebookService.findNoteBookByIdService as jest.Mock
+      ).mockResolvedValueOnce(mockNotebook);
       (
         notebookService.deleteNoteBookService as jest.Mock
       ).mockResolvedValueOnce(mockNotebook);
       const req = {
+        currentUser: {
+          id: "ddd",
+        },
         params: { id: "1" },
       } as unknown as Request;
       const res = {
@@ -161,6 +180,7 @@ describe("Notebook Controller", () => {
 
       await deleteNoteBookController(req, res);
 
+      expect(notebookService.findNoteBookByIdService).toHaveBeenCalledWith("1","ddd");
       expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
       expect(res.json).toHaveBeenCalledWith({
         data: mockNotebook,
@@ -169,5 +189,3 @@ describe("Notebook Controller", () => {
     });
   });
 });
-
-
